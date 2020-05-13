5591254028dd031d710ea7c0feca36c3ec31eb3c
sschuraytz <sbschuraytz@gmail.com>
2020-05-13 10:09:02 -0400

    removed title bar

--- app/src/main/java/com/sschuraytz/charcoaldrawing/MainActivity.java
+++ app/src/main/java/com/sschuraytz/charcoaldrawing/MainActivity.java
@@ -3,12 +3,22 @@ package com.sschuraytz.charcoaldrawing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        View decorView = getWindow().getDecorView();
+        int uiOptions =
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 }
