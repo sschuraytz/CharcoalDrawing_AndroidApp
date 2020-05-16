e56f5cef332becd0ff9132c65b2a619b190760b3
sschuraytz <sbschuraytz@gmail.com>
2020-05-15 21:43:39 -0400

    removed top nav bar and status bar

--- app/src/main/java/com/sschuraytz/charcoaldrawing/MainActivity.java
+++ app/src/main/java/com/sschuraytz/charcoaldrawing/MainActivity.java
@@ -1,24 +1,49 @@
 package com.sschuraytz.charcoaldrawing;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
+    private View decorView;
+    private int uiOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        View decorView = getWindow().getDecorView();
-        int uiOptions =
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-
+        setSystemUI();
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideTopBar();
+    }
+
+    public void setSystemUI()
+    {
+        decorView = getWindow().getDecorView();
+        uiOptions =
+                  View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN  //hide status bar
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        hideTopBar();
+
+        decorView.setOnSystemUiVisibilityChangeListener(
+                new View.OnSystemUiVisibilityChangeListener()
+                {
+                    public void onSystemUiVisibilityChange(int visible)
+                    {
+                       hideTopBar();
+                    }
+                });
+    }
+
+    public void hideTopBar()
+    {
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
